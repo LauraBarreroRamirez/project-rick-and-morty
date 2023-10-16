d2bf235032075552b4b13656f2aa2f39a96bba6f
Laura Barrero <laurabarrero295@gmail.com>
2023-10-16 12:29:10 -0500

    instalacion axios + llamada a la api primera parte

--- src/Tarjetas.jsx
+++ src/Tarjetas.jsx
@@ -1,8 +1,30 @@
+import { useState, useEffect } from "react";
+import s from "./Tarjetas.module.css";
+import axios from "axios";
+
 function Tarjetas() {
+    const [personajes, setPersonajes] = useState([]);
+
+    const fetchData = () => {
+        return axios.get("https://rickandmortyapi.com/api/character") 
+        .then((response) => setPersonajes(response.data.results))
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, [])
     return (
-      <div>
-        <h1>Tarjetas</h1>
-        <button>click me</button>
+      <div className={s.container}>
+        <h1 className={s.titulo}>Tarjetas</h1>
+        <div className={s.card}>
+            <ul>
+                {personajes?.map((personaje) => (<li key={personaje.name}>{personaje.name}</li>))}
+                {personajes?.map((personaje) => (<li key={personaje.image}>{personaje.image}</li>))}
+            </ul>
+        </div>
+        <div>
+        <button className={s.button}>click me</button>
+        </div>
       </div>
     );
   }
